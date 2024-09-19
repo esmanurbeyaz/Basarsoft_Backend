708f579fe7e56952d14ba4e184c125473579068b
esmanur beyaz <esmanurbeyaz66@hotmail.com>
2024-09-19 11:45:56 +0300

    Proje dosyası ekle.

---
+++ Controllers/PointController.cs
@@ -0,0 +1,88 @@
+﻿using Basarsoft1;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PointController : ControllerBase
+    {
+        private static List<Point> _pointList = new List<Point>(); //nesneleri tutan liste
+
+        [HttpGet]
+        public List<Point> GetAll() //tüm point listelerini dönderir
+        {
+            return _pointList;
+        }
+
+
+        [HttpPost]
+        public Point Add(Point point) //point değil void de yapabilirdin
+        {
+            var _point = new Point()
+            {
+                Id = point.Id,
+                Name = point.Name,
+                PointX = point.PointX,
+                PointY = point.PointY,
+            };
+            _pointList.Add(point);
+            return _point;
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Point> GetById(int id)
+        {
+            var point = _pointList.FirstOrDefault(p => p.Id == id);
+            if (point == null)
+            {
+                return NotFound();
+
+            }
+            return point;
+        }
+
+
+
+        [HttpPut("{id}")]
+        public ActionResult Update(int id, Point updatedPoint)
+        {
+            var point = _pointList.FirstOrDefault(p => p.Id == id);
+            if (point == null)
+            {
+                return NotFound();
+            } 
+
+            point.Name = updatedPoint.Name;
+            point.PointX = updatedPoint.PointX;
+            point.PointY = updatedPoint.PointY;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var point = _pointList.FirstOrDefault(point => point.Id == id);
+            if (point == null)
+            {
+                return NotFound();
+            }
+
+           _pointList.Remove(point);
+            return NoContent();
+
+        }
+
+
+
+    }
+
+
+
+    
+}
---
+++ Point.cs
@@ -0,0 +1,45 @@
+﻿namespace Basarsoft1
+{
+    public class Point
+    {
+        public long Id { get; set; }
+        public double PointX { get; set; }
+        public double PointY { get; set; }
+        public string Name { get; set; }
+
+        //class hazır veri tabanında verileri tutarken bu 4 değeri tutacağız
+        //haritaya çağırıp basmak için pointin x ve y sini kullanacağız
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
